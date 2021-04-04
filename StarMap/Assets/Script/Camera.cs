10483ea934afe2d71e152fe4d47bc45fcf5df288
Julian <Julian.begamilrad@gmail.com>
2021-04-04 16:33:59 -0300

    Solar system model base
     6 sides skybox
    randomized start
    Planet prefabs with textures
    camera

---
+++ StarMap/Assets/Script/Camera.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public List<Transform> Planetas;
+
+    private int actualPlanetInSight;
+    private Vector3 NewCameraPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        actualPlanetInSight = 0;
+        NewCameraPosition = Planetas[actualPlanetInSight].transform.position;
+        NewCameraPosition.y += 10;
+        this.transform.position = NewCameraPosition;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (actualPlanetInSight == 8)
+            {
+                actualPlanetInSight = 0;
+            }
+            else
+            {
+                actualPlanetInSight += 1;
+            }
+            
+            NewCameraPosition = Planetas[actualPlanetInSight].transform.position;
+            NewCameraPosition.y += 10;
+            this.transform.position = NewCameraPosition;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (actualPlanetInSight == 0)
+            {
+                actualPlanetInSight = 8;
+            }
+            else 
+            {
+                actualPlanetInSight -= 1;
+            }
+            NewCameraPosition = Planetas[actualPlanetInSight].transform.position;
+            NewCameraPosition.y += 10;
+            this.transform.position = NewCameraPosition;
+            
+        }
+    }
+}
---
+++ StarMap/Assets/Script/SolarSystem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolarSystem : MonoBehaviour
+{
+    public GameObject[] PosiblePlanets;
+    public int actualPlanetInSystem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(PosiblePlanets.Length);
+        List<int> selectedPlanets = new List<int>();
+        int random = 0;
+        
+
+        for (int i = 0; i < actualPlanetInSystem; i++)
+        {
+            do
+            {
+                random = Random.Range(0, PosiblePlanets.Length);
+            } while (selectedPlanets.Contains(random));
+            selectedPlanets.Add(random);
+            
+        }
+        for (int i = 0; i < actualPlanetInSystem; i++)
+        {
+            Instantiate(PosiblePlanets[selectedPlanets[i]]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
