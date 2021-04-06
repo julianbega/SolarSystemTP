389985cf80dad3831e296a8481b399b6260c97da
Julian <Julian.begamilrad@gmail.com>
2021-04-06 20:11:17 -0300

    Meteors
    Meteors:
    Instanciate
    Time Destroy
    Movment
    Collision destroy
    Speed
    Time to new meteor

---
+++ StarMap/Assets/Script/MeteorMovment.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorMovment : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    public float timeToDelete;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeToDelete -= Time.deltaTime;
+        if (timeToDelete <= 0) Destroy(this.gameObject);
+
+        this.gameObject.transform.position -= new Vector3(1.0f, 0.0f, 1.0f) *speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+       Destroy(this.gameObject);
+    }
+}
