8eae8860770134f5256697a4aad396f8deb257b6
Julian <Julian.begamilrad@gmail.com>
2021-04-06 19:32:32 -0300

    Fade
    Planets fade if ship is near
    Earth with atmosphere

---
+++ StarMap/Assets/Script/CollisionFade.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionFade : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        Color color = collision.gameObject.GetComponent<Renderer>().material.color;
+        collision.gameObject.GetComponent<Renderer>().material.color = new Color(color.r, color.g, color.b, 0.5F);
+
+    }
+    private void OnTriggerExit(Collider collision)
+    {
+        Color color = collision.gameObject.GetComponent<Renderer>().material.color;
+        collision.gameObject.GetComponent<Renderer>().material.color = new Color(color.r, color.g, color.b, 1);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
